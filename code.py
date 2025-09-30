25dca7da72e2ce8c8b3cc082acfb0b468f7442e5
Chris Chapman <Chris@SeattleCode.dev>
2025-09-30 02:09:32 -0700

    Code for Segment Tree, dependency injection of function defaults to sum.

--- code.py
+++ code.py
@@ -1,6 +1,76 @@
 # Python3 program to show segment tree operations like construction, query and update
 from math import ceil, log2
 
+
+class SegmentTree:
+    def __init__(self, data, func=sum, initial=0):
+        self.n = len(data)
+        self.size = 2 * (2 ** ceil(log2(self.n))) - 1
+        self.tree = [0] * self.size
+        self._func = func
+        if not isinstance(initial, (list, tuple)):
+            initial = [initial]
+        self.initial = initial
+        self.build(data, 0, self.n - 1, 0)
+
+    def build(self, data, start, end, index):
+        if start == end:
+            self.tree[index] = data[start]
+            return data[start]
+        mid = (start + end) // 2
+        a = self.build(data, start, mid, index * 2 + 1)
+        b = self.build(data, mid + 1, end, index * 2 + 2)
+        self.tree[index] = self._op(a, b)
+        return self.tree[index]
+
+    def _op(self, *args):
+        if not args:
+            return 0
+        return self._func(*self.initial, *args)
+
+    def query(self, left, right):
+        if left < 0 or right > self.n - 1 or left > right:
+            raise ValueError("Invalid Input")
+        return self._query_util(0, self.n - 1, left, right, 0)
+
+    def _query_util(self, start, end, left, right, index):
+        if left <= start and right >= end:
+            return self.tree[index]
+        if end < left or start > right:
+            return 0
+        mid = (start + end) // 2
+        a = self._query_util(start, mid, left, right, 2 * index + 1)
+        b = self._query_util(mid + 1, end, left, right, 2 * index + 2)
+        return self._op(a, b)
+
+    def update(self, idx, value):
+        if idx < 0 or idx > self.n - 1:
+            raise ValueError("Invalid Input")
+        diff = value - self.get_value(idx)
+        self._update_util(0, self.n - 1, idx, diff, 0)
+
+    def _update_util(self, start, end, idx, diff, index):
+        if idx < start or idx > end:
+            return
+        self.tree[index] += diff
+        if start != end:
+            mid = (start + end) // 2
+            self._update_util(start, mid, idx, diff, 2 * index + 1)
+            self._update_util(mid + 1, end, idx, diff, 2 * index + 2)
+
+    def get_value(self, idx):
+        return self._get_value_util(0, self.n - 1, idx, 0)
+
+    def _get_value_util(self, start, end, idx, index):
+        if start == end:
+            return self.tree[index]
+        mid = (start + end) // 2
+        if idx <= mid:
+            return self._get_value_util(start, mid, idx, 2 * index + 1)
+        else:
+            return self._get_value_util(mid + 1, end, idx, 2 * index + 2)
+
+
 def getMid(s, e) :
     """ A utility function to get the middle index from corner indexes. """
     return s + (e - s) // 2
@@ -87,13 +157,17 @@ if __name__ == "__main__" :
     arr = [1, 3, 5, 7, 9, 11]
     n = len(arr)
     # Build segment tree from given array
-    st = constructST(arr, n)
+    # st = constructST(arr, n)
+    st = SegmentTree(arr)
     # Print sum of values in array from index 1 to 3
     print("Sum of values in given range = ",
-                       getSum(st, n, 1, 3))
+    #                   getSum(st, n, 1, 3))
+                       st.query(1, 3))
     # Update: set arr[1] = 10 and update
     # corresponding segment tree nodes
-    updateValue(arr, st, n, 1, 10)
+    # updateValue(arr, st, n, 1, 10)
+    st.update(1, 10)
     # Find sum after the value is updated
     print("Updated sum of values in given range = ",
-                     getSum(st, n, 1, 3), end = "")
+    #                 getSum(st, n, 1, 3), end = "")
+                     st.query(1, 3), end = "")
